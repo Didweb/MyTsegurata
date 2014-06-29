681557eae5349129b675fc0e57421161e9116566
Eduard Pinuaga <info@did-web.com>
2014-06-29 23:27:37 +0200

    Funicoan todo, falta depurar y codificar passwords

--- Tests/PrincipalTest.php
+++ Tests/PrincipalTest.php
@@ -1,106 +1,221 @@
 <?php
-require_once('mySegurata.php');
+require_once('src/mySegurata.php');
 
 class LocaleTest extends PHPUnit_Framework_TestCase
 {
+	public $acceso;
+	
+	public function setup()
+	{
+		$parametros = array('lista'=>'Juan:0,Pepito:1,Antonio:2','listaPSW'=>'Juan:pass1,Pepito:pass2,Antonio:pass3','acceso'=>0,'session'=>'','cookie'=>'pass1:15');
+		$acceso = new mySegurata($parametros);	
+		$this->acceso = $acceso;
+		
+	}
+	
+	
 	public function testExisteArchivo()
 	{
-		$this->assertFileExists('mySegurata.php');
+		$this->assertFileExists('src/mySegurata.php');
 	}
 	
 	public function testInstanciarObjeto()
-	{
-		$this->assertInstanceOf('mySegurata', new mySegurata);
+	{	$parametros = array('lista'=>'Juan:0,Pepito:1,Antonio:2','listaPSW'=>'Juan:pass1,Pepito:pass2,Antonio:pass3','acceso'=>0,'session'=>'','cookie'=>'pass1:15');
+		$this->assertInstanceOf('mySegurata', new mySegurata($parametros));
 	}
-	
+
+
+
+	public function testParametrosConctructor()
+	{
+		
+		$this->assertInternalType('string', $this->acceso->getIn_lista());
+		$this->assertInternalType('string', $this->acceso->getIn_listaPSW());
+		$this->assertInternalType('integer', $this->acceso->acceso);
+	}
+
 	public function testAccesoSetyGet()
 	{
-		$acceso = new mySegurata();
-		$acceso->setAcceso('1');
 		
-		$this->assertEquals('1', $acceso->getAcceso());
+		$this->acceso->setAcceso('1');
+		$this->assertEquals('1', $this->acceso->getAcceso());
 	}
+
+
+	public function testOpcionCookieGet()
+	{
+		// sin definir opcion de cookie
+		$opcion = $this->acceso->getOpcionCookie('1');
+		$this->assertEquals('0', $opcion);
+		
+		//definiendo opcion cookie en 1 = si
+		$parametros = array('lista'=>'Juan:0,Pepito:1,Antonio:2','listaPSW'=>'Juan:pass1,Pepito:pass2,Antonio:pass3','acceso'=>0,'session'=>'','cookie'=>'','opcionCookie'=>1);
+		$acceso2 = new mySegurata($parametros);	
+		
+		
+	}
+
 
 	public function testCrearLista()
 	{
-		$acceso = new mySegurata();
 		
 		//comprobamos que devuelva un array
-		$lista = $acceso->CrearLista('Juan:0,Pepito:1,Antonio:2');
+		$lista = $this->acceso->CrearLista('Juan:0,Pepito:1,Antonio:2');
 		$this->assertInternalType('array', $lista);
 		
 		// En el caso de que no escriban bien la lista
-		$lista = $acceso->CrearLista(',Juan:0,Pepito:1,Antonio:2,');
+		$lista = $this->acceso->CrearLista(',Juan:0,Pepito:1,Antonio:2,');
 		$this->assertEquals('2', $lista['Antonio']);
 		
 		// En el caso de que no escriban bien la lista, que registre los valores validos.
-		$lista = $acceso->CrearLista(',Juan:0,Pepito:1,Antonio:2,');
+		$lista = $this->acceso->CrearLista(',Juan:0,Pepito:1,Antonio:2,');
 		$this->assertCount(3, $lista);
 		
 		// En el caso de que no escriban bien la lista, que registre los valores validos.
-		$lista = $acceso->CrearLista('Juan:,Pepito:1,Antonio:2,');
+		$lista = $this->acceso->CrearLista('Juan:,Pepito:1,Antonio:2,');
 		$this->assertCount(2, $lista);
 		
 		// En el caso de que no escriban bien la lista, que registre los valores validos.
-		$lista = $acceso->CrearLista(':,Pepito:1,Antonio:2,');
+		$lista = $this->acceso->CrearLista(':,Pepito:1,Antonio:2,');
 		$this->assertCount(2, $lista);
+		
+		//En el caso de no tener ningún valor bien.
+		$lista = $this->acceso->CrearLista('xxxx');
+		$this->assertNull(null,$lista);
+		
 	}
 
 
 
 	public function testListaSetyGet()
 	{
-		$acceso = new mySegurata();
+		
 		
 		// Comprobar Set y Get de ListaPSW
-		$acceso->setListaPSW('hola');
-		$lista = $acceso->getListaPSW();
-		$this->assertEquals('hola',$lista);
+		$this->acceso->setListaPSW('hola');
+		$lista = $this->acceso->getListaPSW();
+		$this->assertNull(null,$lista);
 		
 		// Comprobar Set y Get de Lista
-		$acceso->setLista('hola');
-		$lista = $acceso->getLista();
-		$this->assertEquals('hola',$lista);
+		$this->acceso->setLista('hola');
+		$lista = $this->acceso->getLista();
+		$this->assertNull(null,$lista);
+		
+		
+		// Comprobar Set y Get de ListaPSW
+		$this->acceso->setListaPSW('Juan:pass1,Pepito:pass2,Antonio:pass3');
+		$lista = $this->acceso->getListaPSW();
+		$this->assertInternalType('array', $lista);
+		
+		// Comprobar Set y Get de Lista
+		$this->acceso->setLista('Juan:0,Pepito:1,Antonio:2');
+		$lista = $this->acceso->getLista();
+		$this->assertInternalType('array', $lista);
+		
 	}
 
-	
-	public function testCheckeo()
+	public function testUserSetyGet()
 	{
-		$acceso = new mySegurata();
-		
-		// entrada null
-		$check = $acceso->checkeo();
-		$this->assertNull($check);
-		
-		// Comprobamos que devuelve el pase correcto
-		$lista 		= $acceso->CrearLista('Juan:0,Pepito:1,Antonio:2');
-		$listaPSW 	= $acceso->CrearLista('Juan:pass1,Pepito:pass2,Antonio:pass3');
-		
-		$acceso->setLista($lista);
-		$acceso->setListaPSW($listaPSW);
-		
-		$check = $acceso->checkeo('Juan','pass1');
-		$this->assertEquals('0',$check);
-
-
-		// Probamos con valores nulos.
-		$check = $acceso->checkeo('Juan');
-		$this->assertEquals(null,$check);
-		
-		// Valores incorrectos en usuario y password.
-		$check = $acceso->checkeo('xxxx','yyyy');
-		$this->assertEquals(null,$check);
-
+		$user = $this->acceso->setUser('Manolo');
+		$user = $this->acceso->getUser();
+		$this->assertEquals('Manolo',$user);
 	}	
+
 	
 	public function testVisita()
 	{
-		$acceso = new mySegurata();
-
-		// entrada null
-		$visita = $acceso->visita();
-		$this->assertNull($visita);
+		// entrada con parametros password y usuario null
+		$parametros = array('lista'=>'Juan:0,Pepito:1,Antonio:2','listaPSW'=>'Juan:pass1,Pepito:pass2,Antonio:pass3','acceso'=>0,'session'=>'','cookie'=>'');
+		$acceso2 = new mySegurata($parametros);	
+		$visita = $acceso2->visita();
+		$this->assertEquals(0,$visita);
+		$this->assertInternalType('integer', $visita);
+		
+		// entrada por visita con acceso permitido
+		$parametros = array('lista'=>'Juan:0,Pepito:1,Antonio:2','listaPSW'=>'Juan:pass1,Pepito:pass2,Antonio:pass3','acceso'=>0,'session'=>'','cookie'=>'');
+		$acceso2 = new mySegurata($parametros);
+		$visita = $acceso2->visita('Pepito','pass2');
+		$this->assertEquals(1, $visita);
+		$this->assertInternalType('integer', $visita);
+		
+		// con uno d elos parametros (usuario o password) en null
+		$parametros = array('lista'=>'Juan:0,Pepito:1,Antonio:2','listaPSW'=>'Juan:pass1,Pepito:pass2,Antonio:pass3','acceso'=>0,'session'=>'','cookie'=>'');
+		$acceso2 = new mySegurata($parametros);
+		$visita = $acceso2->visita('Pepito');
+		$this->assertEquals(0, $visita);
+		$this->assertInternalType('integer', $visita);
+		
+		// con un valor de cookie incorrecto
+		$parametros = array('lista'=>'Juan:0,Pepito:1,Antonio:2','listaPSW'=>'Juan:pass1,Pepito:pass2,Antonio:pass3','acceso'=>0,'session'=>'','cookie'=>'xxxx');
+		$acceso2 = new mySegurata($parametros);
+		$visita = $acceso2->visita();
+		$this->assertEquals(0, $visita);
+		$this->assertInternalType('integer', $visita);
+		
+		// con un valor de session incorrecto
+		$parametros = array('lista'=>'Juan:0,Pepito:1,Antonio:2','listaPSW'=>'Juan:pass1,Pepito:pass2,Antonio:pass3','acceso'=>0,'session'=>'yyyyy','cookie'=>'');
+		$acceso2 = new mySegurata($parametros);
+		$visita = $acceso2->visita();
+		$this->assertEquals(0, $visita);
+		$this->assertInternalType('integer', $visita);
+		
+		
+		// con valor cookie correcto y sin sesion
+		$parametros = array('lista'=>'Juan:15,Pepito:1,Antonio:2','listaPSW'=>'Juan:pass1,Pepito:pass2,Antonio:pass3','acceso'=>0,'session'=>'','cookie'=>'pass1:15');
+		$acceso2 = new mySegurata($parametros);
+		$acceso2->visita();
+		$elacceso = $acceso2->getAcceso();
+		$this->assertEquals(15, $elacceso);
+		$this->assertInternalType('integer', $elacceso);
+		
+		
+		// con un valor de session incorrecto
+		$parametros = array('lista'=>'Juan:0,Pepito:1,Antonio:2','listaPSW'=>'Juan:pass1,Pepito:pass2,Antonio:pass3','acceso'=>0,'session'=>'2','cookie'=>'');
+		$acceso2 = new mySegurata($parametros);
+		$acceso2->visita();
+		$elacceso = $acceso2->getAcceso();
+		$this->assertEquals(2, $elacceso);
+		$this->assertInternalType('integer', $elacceso);
+		
+		
 	}		
+		
+	public function testResultadEntrada()
+	{
+		// Comprobar Set y Get de ListaPSW
+		$parametros = array('lista'=>'Juan:0,Pepito:1,Antonio:2','listaPSW'=>'Juan:pass1,Pepito:pass2,Antonio:pass3','acceso'=>0,'session'=>'','cookie'=>'');
+		$acceso2 = new mySegurata($parametros);
+		$entrada = $this->acceso->getRes_entrada();
+		$this->assertEquals(0,$entrada);
+	}		
+	
+		
+	public function testComprobarPase()
+	{
+		// valores correctos en cookie
+		$pase = $this->acceso->comprobarPase('pass2:1');
+		$this->assertEquals(1, $pase);
+		
+		// valores incorrectos
+		$pase = $this->acceso->comprobarPase('xxx');
+		$this->assertEquals(0, $pase);
+		
+		// valores inexistentes
+		$pase = $this->acceso->comprobarPase('xxx:1');
+		$this->assertEquals(0, $pase);
+		
+	}	
+
+
+
+	public function testAccesos()
+	{
+		// Comprobar Set y Get de ListaPSW
+		$parametros = array('lista'=>'','listaPSW'=>'','acceso'=>0,'session'=>'','cookie'=>'');
+		$acceso2 = new mySegurata($parametros);
+		$visita = $acceso2->visita();
+		$this->assertEquals(0,$visita);
+	}	
 	
 }
 ?>	
---
+++ src/mySegurata.php
@@ -0,0 +1,251 @@
+<?php
+
+class mySegurata 
+{
+	public $acceso;
+	private $lista;
+	private $listaPSW;
+	private $user;
+	
+	private $in_lista;
+	private $in_listapsw;
+	
+	private $res_entrada;
+	private $session;
+	private $cookie;
+	private $opcionCookie;
+	private $cookieExpira = 3600;
+	
+	public function __construct($parametros)
+	{
+	  $this->in_lista 		= $parametros['lista'];
+	  $this->in_listaPSW 	= $parametros['listaPSW']; 
+	  $this->acceso 		= $parametros['acceso']; 
+	  $this->session 		= $parametros['session']; 
+	  $this->cookie 		= $parametros['cookie']; 
+	  
+	  
+	  if(isset($parametros['opcionCookie'])) {
+				$this->opcionCookie	= $parametros['opcionCookie'];
+				} else {
+				$this->opcionCookie	= 0;  }
+	   
+	}
+	
+	
+	public function setAcceso($valor)
+	{
+		$this->acceso = $valor;
+		return $this;
+	}
+	
+	
+	public function getAcceso()
+	{
+		return (int)$this->acceso;
+	}
+
+	public function getUser()
+	{
+		return $this->user;
+	}
+
+	public function setUser($valor)
+	{
+		$this->user = $valor;
+		return $this;
+	}
+	
+	public function getIn_lista()
+	{
+		return $this->in_lista;
+	}
+	
+	public function getIn_listaPSW()
+	{
+		return $this->in_listaPSW;
+	}
+	
+	public function setRes_entrada($valor)
+	{
+		$this->res_entrada = $valor;
+		return $this;
+	}	
+	
+	public function getRes_entrada()
+	{
+		return $this->res_entrada;
+	}
+	
+	public function getSession()
+	{
+		return $this->session;
+	}
+	
+	
+	public function getCookie()
+	{
+		return $this->cookie;
+	}
+	
+	
+	public function getOpcionCookie()
+	{
+		return $this->opcionCookie;
+	}	
+	public function CrearLista($valor)
+	{
+		$lista = array();
+		$lista1 = explode(',',$valor);
+		foreach ($lista1 as $nom=>$val){
+			
+			if(strpos($lista1[$nom], ':')){
+				$lista2 = explode(':',$lista1[$nom]);
+				
+					foreach ($lista2 as $nom2=>$val2){
+						
+						if($lista2[0]=='' || $lista2[1]==''){
+							
+							} else {
+								
+							$lista[$lista2[0]] = $lista2[1];
+							}
+						}
+					} 	
+				}
+
+		return $lista;
+	}
+	
+	
+	public function setLista($lista)
+	{
+		$this->lista = $this->CrearLista($lista);
+		return $this->lista;
+	}
+	
+	
+	public function getLista()
+	{
+		return $this->lista;
+	}
+
+
+	public function setListaPSW($listaPSW)
+	{
+		$this->listaPSW = $this->CrearLista($listaPSW);
+		return $this->listaPSW;
+	}
+	
+	
+	public function getListaPSW()
+	{
+		return $this->listaPSW;
+	}
+
+	public function comprobarPase($valor)
+	{
+		if(strpos($valor, ':')){
+			$des = explode(':',$valor); 
+			
+			$this->setListaPSW($this->getIn_listaPSW());
+			$listadoPSW = $this->getListaPSW();
+			
+			$this->setLista($this->getIn_lista());
+			$lista = $this->getLista();
+			
+			$usuario = array_search($des[0],$listadoPSW);
+			
+			if($usuario != FALSE && $des[1] == $lista[$usuario]) { 
+				$this->setUser(array_search($des[0],$listadoPSW));
+				$this->setAcceso($des[1]);
+				return 1;	
+				}
+			$this->setAcceso(0);
+			return 0;
+			} else {
+			$this->setAcceso(0);	
+			return 0;	
+			}
+	}
+
+
+
+	public function visita($usuario = null,$password = null)
+	{
+		$session = $this->getSession();
+		$cookie  = $this->getCookie();
+		
+		if($session != '') {
+			
+			$this->setAcceso($session);
+			$this->res_entrada = 0;
+			return $this->getAcceso(); 
+			} elseif ($session == '') {
+				
+				if($cookie != ''){
+					$this->res_entrada = 0;
+					
+					$this->comprobarPase($cookie);
+					return $this->getAcceso(); 
+					
+					} elseif ($cookie == '') {
+					
+						$this->setLista($this->getIn_lista());
+						$this->setListaPSW($this->getIn_listaPSW());
+					
+						$lista 		= $this->getLista();
+						$listaPSW 	= $this->getListaPSW();
+						
+							if (!empty($lista) && !empty($listaPSW)) {
+								
+								$this->setAcceso($this->checkeo($usuario,$password));
+								
+								if($this->getOpcionCookie()==1) {
+									setcookie("pass", $password.':'.$this->getAcceso(), time() + $this->cookieExpira);  }
+								
+								return $this->getAcceso();	
+								} else { 
+									$this->setAcceso(0);
+									return $this->getAcceso(); 
+									}
+						} 
+						}
+	}
+
+	
+	public function checkeo($usuario,$password)
+	{
+		if($usuario != null && $password != null){
+				
+				
+			$lista_psw = $this->getListaPSW();	
+			
+			foreach ($lista_psw as $nom=>$val){
+				
+				$key = $nom;
+				$us_encontrado = array_search($usuario,$lista_psw);
+				
+				if ($us_encontrado == FALSE){
+					if($usuario == $key ){
+							if ($password == $lista_psw[$key]) {
+									$this->setRes_entrada(1);
+									$this->setUser($usuario);
+									return 	(int)$this->lista[$usuario]; 
+							} 
+						}
+				
+						} else {
+						$this->setRes_entrada(0);
+						return 0;
+						}
+			} 
+			$this->setRes_entrada(0);
+			return 0;	
+				
+				} else {
+			$this->setRes_entrada(0);
+			return 0; } }
+	
+}
+?>
